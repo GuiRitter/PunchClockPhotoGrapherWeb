1100193395268bb756601a37c11d8b792901e873
Guilherme Alan Ritter <gui.a.ritter@gmail.com>
2023-04-07 17:12:10 -0300

    WIP view finder fix

--- src/ui/Photo.js
+++ src/ui/Photo.js
@@ -60,8 +60,8 @@ function componentDidUpdate(props/*, prevProps*/, dispatch, videoField, canvasFi
 				document.querySelector(':root').style.setProperty(
 					'--view-finder-box-shadow',
 					(height > width)
-						? `inset ${letterBox}px ${letterBox + windowBox}px #00000080, inset -${letterBox}px -${letterBox + windowBox}px #00000080`
-						: `inset ${letterBox + windowBox}px ${letterBox}px #00000080, inset -${letterBox + windowBox}px -${letterBox}px #00000080`
+						? `inset ${windowBox}px ${letterBox + windowBox}px #00000080, inset -${windowBox}px -${letterBox + windowBox}px #00000080`
+						: `inset ${letterBox + windowBox}px ${windowBox}px #00000080, inset -${letterBox + windowBox}px -${windowBox}px #00000080`
 				);
 
 				videoField.setAttribute('width', width);
